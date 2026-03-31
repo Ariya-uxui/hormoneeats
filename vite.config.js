e223287acfdfd4945ba6d899fbb2b1f325064d75
Ariya <ttkonly13@gmail.com>
2026-03-31 19:07:25 +0700

    fix: remove base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/hormoneeats/",
   root: ".",
   server: {
     port: 3000,
@@ -12,13 +11,5 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
-    chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        }
-      }
-    }
   },
 })
