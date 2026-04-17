489951cdb27312bc5911fa7585a7cb1b4bc0e92a
Ariya <ttkonly13@gmail.com>
2026-04-17 14:22:35 +0700

    fix: remove duplicate defineConfig import

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite"
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   publicDir: "public",
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-  },
+  server: { port: 3000, open: true },
+  build: { outDir: "dist", sourcemap: false },
 })
